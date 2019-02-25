e396c2b9d76e869e3361be5cb05699bf9fb395bf
amepila <ie704453@iteso.mx>
2019-02-24 18:43:49 -0600

    Functional Version TOP

--- blinking_machine/TOP_blinking_machine.sv
+++ blinking_machine/TOP_blinking_machine.sv
@@ -10,29 +10,35 @@ module TOP_blinking_machine(
 
 wire 	   w_clk_1hz;
 wire 	   w_timeout;
+wire 		w_flag_counter;
 wire [2:0] w_count_ena;
 
-clk_divider CD(
+clk_divider CD
+(
 	.i_rst(i_rst), 
 	.i_clk_FPGA(i_clk),
 	
 	.o_clk(w_clk_1hz)
 );
 
-blinking_SM SM(
+blinking_SM SM
+(
 	.i_clk(w_clk_1hz),
 	.i_rst(i_rst),
 	.i_start(i_start),
-	.i_ena(w_timeout),
+	.i_enable(w_timeout),
 
 	.o_out(o_out),
+	.o_flag_counter(w_flag_counter),
 	.o_count_ena(w_count_ena)
 );
 
-counter COUNT(
+counter COUNT
+(
 	.i_clk(w_clk_1hz),
 	.i_rst(i_rst),
 	.i_val(w_count_ena),
+	.i_enable(w_flag_counter),
 
 	.o_timeout(w_timeout)
 );
