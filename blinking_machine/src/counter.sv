a5c9e7c788a1a1ae5c80381f0476d08157a74b47
amepila <ie704453@iteso.mx>
2019-02-24 21:27:52 -0600

    Documentation Counter

--- blinking_machine/src/counter.sv
+++ blinking_machine/src/counter.sv
@@ -1,6 +1,24 @@
+/*******************************************************************
+* Nombre del modulo:
+*  counter.sv
+* Descripcion:
+*  Este modulo representa el contador
+* Entradas:
+*  Clock, reset, enable; el cual representa la bandera para poder
+*  contar, y val; esta valor representa los valores a contar
+* Salidas: 
+*  Una bandera de timeout al terminar de contar
+* Version: 
+*  1.0
+* Autores:
+*  Carem Bernabe
+*  Andres Hernandez
+* Fecha: 
+*  24/02/2019
+********************************************************************/
 module counter 
 #( 
-	parameter COUNT_LENGTH  =  $clog2(6) 
+	parameter COUNT_LENGTH  =  $clog2(6) // Ajusta el nivel de bits necesarios para el conteo
 )
 (
 	input 					    		i_clk,
@@ -12,7 +30,7 @@ module counter
 );
 
 
-	logic 					    l_timeout = 0;
+	logic 					    l_timeout = 0; // Bandera de conteo
 	logic [COUNT_LENGTH-1:0] l_counter = 0; // Frequency counter
 	
 
@@ -21,17 +39,17 @@ always@(posedge i_clk, posedge i_rst) begin
 	if(i_rst)// Reset counter
 		l_counter <= 0;
 	else if(i_enable)
-		if(l_counter >= i_val -1'b1)
-			l_counter <= 1'b0;
+		if(l_counter >= i_val -1'b1) //Si el conteo llega al limite se reinicia
+			l_counter <= 1'b0; 
 		else
-			l_counter <= l_counter + 1'b1;
+			l_counter <= l_counter + 1'b1; //Aumenta el contador en 1
 end
 
 always_comb begin
 	if(l_counter >= i_val - 1'b1)
-		l_timeout = 1'b1;
+		l_timeout = 1'b1; //Cuando el contador llega al limite, activa la bandera
 	else
-		l_timeout = 1'b0;
+		l_timeout = 1'b0; //Si aun no termina, la bandera no se activa
 end
 
 assign o_timeout = l_timeout;
