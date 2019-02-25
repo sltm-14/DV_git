719cfe0971d22b7cc1227ac2644bd8ad815d3c77
amepila <ie704453@iteso.mx>
2019-02-24 18:43:04 -0600

    Functional Version Counter

--- blinking_machine/counter.sv
+++ blinking_machine/counter.sv
@@ -3,12 +3,13 @@ module counter
 	parameter COUNT_LENGTH  =  $clog2(6) 
 )
 (
-	input 					     i_clk,
-	input 					     i_rst,
-	input  [COUNT_LENGTH-1:0]    i_val,
+	input 					    		i_clk,
+	input 					     		i_rst,
+	input 								i_enable,
+	input  [COUNT_LENGTH-1:0]    	i_val,
 
-	output					     o_timeout
-	);
+	output					     		o_timeout
+);
 
 
 	logic 					    l_timeout = 0;
@@ -17,24 +18,20 @@ module counter
 
 always@(posedge i_clk, posedge i_rst) begin
 
-	if(i_rst) begin // Reset counter
+	if(i_rst)// Reset counter
 		l_counter <= 0;
-	end
+	else if(i_enable)
+		if(l_counter >= i_val -1'b1)
+			l_counter <= 1'b0;
+		else
+			l_counter <= l_counter + 1'b1;
+end
 
-	else if( (i_val > 0) && l_counter < i_val -1'b1)begin
-		l_counter <= l_counter + 1'b1;
-		l_timeout <= 1'b0;
-	end
-
-	else if (i_val == 0)
-		l_timeout <= 1'b0;
-
-	else begin
-
-		l_counter <= 0;
-		l_timeout <= 1'b1;
-	end 
-
+always_comb begin
+	if(l_counter >= i_val - 1'b1)
+		l_timeout = 1'b1;
+	else
+		l_timeout = 1'b0;
 end
 
 assign o_timeout = l_timeout;
