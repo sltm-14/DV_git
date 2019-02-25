c3b26cc7ccff9d96ab0b187d41a4d07d42063857
amepila <ie704453@iteso.mx>
2019-02-24 18:42:20 -0600

    Functional Version SM

--- blinking_machine/blinking_SM.sv
+++ blinking_machine/blinking_SM.sv
@@ -1,82 +1,94 @@
-module blinking_SM(
+module blinking_SM
+(
 	input        i_clk,
 	input        i_rst,
 	input        i_start,
-	input        i_ena,
+	input 		 i_enable,
 
 	output       o_out,
+	output		 o_flag_counter,
 	output [2:0] o_count_ena
 	);
 
-enum logic [2:0] {IDLE, ON_1, OFF_1, ON_2, OFF_2, ON_3, OFF_3} state, next_state = IDLE; 
+enum logic [2:0] {IDLE, ON_1, OFF_1, ON_2, OFF_2, ON_3, OFF_3} state; 
 
 logic       l_out;
+logic			l_flag_counter;
 logic [2:0] l_count_ena;
 
- always @ (posedge i_clk, posedge i_rst)begin
-     state <= (i_rst)? IDLE : next_state;
- end
+ always_ff@(posedge i_clk, posedge i_rst)begin
 
- always @(posedge i_clk)begin
- 
+		if(i_rst)
+			state <= IDLE;
+		else
 	 
 		case (state)
-			IDLE: begin
-				if(i_start)
-					next_state <= ON_1;
-				else 
-					next_state <= next_state;
-			end
+			IDLE:
+				if(i_start == 1'b1)
+					state <= ON_1;
+				else
+					state <= IDLE;
 
-			ON_1:  begin
-				next_state = (i_ena)? OFF_1 : next_state;
-			end
+			ON_1:
+				if(i_enable == 1'b0)
+					state <= ON_1;
+				else
+					state <= OFF_1;
+				
+			OFF_1:
+				if(i_enable == 1'b0)
+					state <= OFF_1;
+				else
+					state <= ON_2;
+			
+			ON_2:
+				if(i_enable == 1'b0)
+					state <= ON_2;
+				else
+					state <= OFF_2;
+				
+			OFF_2:
+				if(i_enable == 1'b0)
+					state <= OFF_2;
+				else
+					state <= ON_3;
+					
+			ON_3:
+				if(i_enable == 1'b0)
+					state <= ON_3;
+				else
+					state <= OFF_3;
+				
+			OFF_3:
+				if(i_enable == 1'b0)
+					state <= OFF_3;
+				else
+					state <= IDLE;
+			default:
+					state = IDLE;
 
-			OFF_1: begin
-				next_state = (i_ena)? ON_2  : next_state;
-			end
-
-			ON_2: begin
-				next_state = (i_ena)? OFF_2 : next_state;
-			end
-
-			OFF_2: begin
-				next_state = (i_ena)? ON_3 : next_state;
-			end
-
-			ON_3: begin
-				next_state = (i_ena)? OFF_3 : next_state;
-			end
-
-			OFF_3: begin
-				next_state = (i_ena)? IDLE : next_state;
-			end
-
-			default:begin
-				next_state = (i_ena)? IDLE : next_state;
-			end
-
-		endcase // state
-		 
-
+		endcase // state		 
  end
 
- always @ (*)begin
- 	if ( state == ON_1 || state == ON_2 || state == ON_3 )begin
- 		l_out       = 1'b1;
- 		l_count_ena = 3'b110;
- 	end
- 	else if ( state == OFF_1 || state == OFF_2 || state == OFF_3 )begin
- 		l_out       = 1'b0;
- 		l_count_ena = 3'b100;
- 	end
- 	else begin
- 		l_out       = 1'b0;
- 		l_count_ena = 3'b000;
- 	end
-
+ always_comb begin
+	if(state == ON_1 || state == ON_2 || state == ON_3)begin
+		l_flag_counter = 1'b1;
+		l_out       = 1'b1;
+		l_count_ena = 3'b110;
+	end
+	else if(state == OFF_1 || state == OFF_2 || state == OFF_3)begin
+		l_flag_counter = 1'b1;
+		l_out       = 1'b0;
+		l_count_ena = 3'b100;
+	end
+	else	begin
+		l_flag_counter = 1'b0;
+		l_out       = 1'b0;
+		l_count_ena = 3'b000;
+	end
  end
  
+ assign o_flag_counter = l_flag_counter;
  assign o_out       = l_out;
  assign o_count_ena = l_count_ena;
 
