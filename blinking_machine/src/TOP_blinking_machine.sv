8aff6ffaa639bcf12eda98a3bb4fa8b3dddff146
amepila <ie704453@iteso.mx>
2019-02-24 21:05:44 -0600

    Documentation TOP

--- blinking_machine/src/TOP_blinking_machine.sv
+++ blinking_machine/src/TOP_blinking_machine.sv
@@ -1,18 +1,38 @@
-
+/*******************************************************************
+* Nombre del modulo:
+*  TOP_blinking_machine.sv
+* Descripcion:
+*  Este modulo representa el Top module del sistema
+* Entradas:
+*  Clock, reset y start, el cual representa a la entrada de un switch
+*  que inicia con el conteo de los estados.
+* Salidas: 
+*  Senal del conteo a 1Hz en un led verde y la salida del estado se
+*  reflejara en un led rojo
+* Version: 
+*  1.0
+* Autores:
+*  Carem Bernabe
+*  Andres Hernandez
+* Fecha: 
+*  24/02/2019
+********************************************************************/
 module TOP_blinking_machine(
 	input  i_clk,
-	input  i_rst,
+	input  i_rst,	
 	input  i_start,
 
 	output o_out,
 	output o_clk_1hz
 	);
-
+	
+//Wires para instanciar
 wire 	   w_clk_1hz;
 wire 	   w_timeout;
 wire 		w_flag_counter;
 wire [2:0] w_count_ena;
 
+//Instancia para clock divider
 clk_divider CD
 (
 	.i_rst(i_rst), 
@@ -21,6 +41,7 @@ clk_divider CD
 	.o_clk(w_clk_1hz)
 );
 
+//Instancia para la maquina de estados
 blinking_SM SM
 (
 	.i_clk(w_clk_1hz),
@@ -33,6 +54,7 @@ blinking_SM SM
 	.o_count_ena(w_count_ena)
 );
 
+//Instancia para el contador
 counter COUNT
 (
 	.i_clk(w_clk_1hz),
