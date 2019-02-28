eb7087198243934b8d0ac29d0438bd6f386d112b
sltm-14 <ie693242@iteso.mx>
2019-02-28 17:24:27 -0600

    mux

---
+++ Registers/universal_shift/mux.sv
@@ -0,0 +1,15 @@
+module mux #(
+	PARAMETER SLC = 2,
+	PARAMETER DW  = 4
+	)(
+	input 				clk,
+	input 				rst,
+	input  [SLC-1:0]	i_slc,
+	input  [DW-1:0]     i_data
+
+	output 			 	o_out
+	);
+
+	assign o_out = i_data[i_slc];
+
+endmodule
