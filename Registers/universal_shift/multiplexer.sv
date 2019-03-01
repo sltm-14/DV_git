eb8efdcc31f0711097ae835496ab8583085bfe2f
amepila <ie704453@iteso.mx>
2019-02-28 18:32:02 -0600

    New Mux

--- Registers/universal_shift/multiplexer.sv
+++ Registers/universal_shift/multiplexer.sv
@@ -1,11 +1,13 @@
-module mux #(
+module multiplexer
+ #(
 	PARAMETER SLC = 2,
 	PARAMETER DW  = 4
-	)(
+	)
+	(
 	input 				clk,
 	input 				rst,
 	input  [SLC-1:0]	i_slc,
-	input  [DW-1:0]     i_data
+	input  [DW-1:0]     i_data,
 
 	output 			 	o_out
 	);
