4b0a2dd3377fce53c43da0b10964f170e73ba6c3
amepila <ie704453@iteso.mx>
2019-03-01 19:14:23 -0600

    Documentation of SISO Right

--- Registers/siso/src/siso_right.sv
+++ Registers/siso/src/siso_right.sv
@@ -1,10 +1,31 @@
+/*******************************************************************
+* Nombre del modulo:
+*  siso_right.sv
+* Descripcion:
+*  Este modulo representa un registro SISO, el cual, es una entrada 
+*  serial y una salida serial
+* Entradas:
+*  Clock, reset, selector, enable y entrada serial
+* Salidas: 
+*  Salida serial
+* Version: 
+*  1.0
+* Autores:
+*  Carem Bernabe
+*  Andres Hernandez
+* Fecha: 
+*  01/03/2019
+********************************************************************/
 module siso_right #(
 	parameter DW = 4
 	)(
+	//Inputs
 	input               clk,
 	input               rst,
 	input               enb,
 	input               inp,
+	
+	//Outputs
 	output              out
 	);
 
@@ -12,9 +33,9 @@ module siso_right #(
 
 	always_ff@(posedge clk or negedge rst) begin: rgstr_label
 	    if(!rst)
-	        rgstr_r  <= '0;
+	        rgstr_r  <= '0;	//Inicia en cero
 	    else if (enb)
-	        rgstr_r  <= {inp,rgstr_r[DW-1:1]};
+	        rgstr_r  <= {inp,rgstr_r[DW-1:1]};//Recorre a la derecha
 	end:rgstr_label
 
 	assign out  = rgstr_r[0];
