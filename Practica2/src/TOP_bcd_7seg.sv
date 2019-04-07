f18f7a2e2367a4ce8a634dd2327b8f34856819e7
amepila <ie704453@iteso.mx>
2019-04-07 17:10:42 -0500

    top bcd with interface

--- Practica2/src/TOP_bcd_7seg.sv
+++ Practica2/src/TOP_bcd_7seg.sv
@@ -14,17 +14,16 @@
 module TOP_bcd_7seg
 import pkg_bin_to_thto::*;
 (
-	input  t_iv       in_Bin,       /* Valor en complemento A2 */
-
-	output t_display  out_displays, /* Salida para los displays */
-	output            out_Signo     /* Salida signo */
+	mdr_if.bcd		bcd_if,			/* Valor en complemento A2 */
+	output t_display	out_displays_result, 	/* Salida para los displays */
+	output           	out_Signo     		/* Salida signo */
 );
 	display_if	top_bcd_itf();
 	 
     /* Modulo para obtener el complemento A2 */
 	complement_a2 A2_COM
 	(
-		.i_Val    		(in_Bin),
+		.i_Val    		(bcd_if.i_bin),
 
 		.o_Signo  		(out_Signo),
 		.complement_if		(top_bcd_itf.complement)
@@ -40,7 +39,7 @@ import pkg_bin_to_thto::*;
     displays DISPLAY
 	 (
 		.display_module_if	(top_bcd_itf.display),
-		.o_7seg     		(out_displays)
+		.o_7seg     		(out_displays_result)
     );
 	
 endmodule
