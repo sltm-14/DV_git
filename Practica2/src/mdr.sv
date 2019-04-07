d189a883988c460293d6493be1b6bd58ef3bb54e
amepila <ie704453@iteso.mx>
2019-04-07 17:12:08 -0500

    mdr with top_bcd

--- Practica2/src/mdr.sv
+++ Practica2/src/mdr.sv
@@ -15,20 +15,22 @@
 
 module mdr
 import system_mdr_pkg::*;
+import pkg_bin_to_thto::*;
 (
-	input 			clk,
-	input 			rst,
-	input 			i_load,
-	input op_t 		i_op,
-	input 			i_start,
-	input data_t 	i_data,
+	input 				clk,
+	input 				rst,
+	input 				i_load,
+	input op_t 			i_op,
+	input 				i_start,
+	input data_t 			i_data,
 	
-	output 			o_ready,
-	output 			o_loadX,
-	output 			o_loadY,
-	output 			o_error
-	output data_t 	o_reaminder,
-	output data_t 	o_result,
+	output 				o_ready,
+	output 				o_loadX,
+	output 				o_loadY,
+	output 				o_error
+	output t_display 		o_displays_result,
+	//output t_display	o_displays_remainder,
+	output            		o_sign 
 
 );
 
@@ -39,24 +41,24 @@ switch SWITCH
 	.clk				(clk),
 	.rst				(rst),
 	.i_sw				(i_data),
-	.switch_if		(top_if.switch)
+	.switch_if			(top_itf.switch)
 );
 
 control CONTROL
 (
 	.clk				(clk),
 	.rst				(rst),
-	.i_load			(i_load),
+	.i_load				(i_load),
 	.i_op				(i_op),
 	.i_start			(i_start),
-	.control_if		(top_itf.control)
+	.control_if			(top_itf.control)
 );
 
 validation VALIDATION
 (
 	.clk				(clk),
 	.rst				(rst),
-	.validation_if	(top_itf.validation)
+	.validation_if			(top_itf.validation)
 );
 
 core MDR
@@ -64,21 +66,27 @@ core MDR
 	.clk				(clk),
 	.rst				(rst),
 	.core_if			(top_itf.core)
-	
-	.o_result		(o_result),
-	.o_remainder	(o_remainder)
 );
 
 leds LEDS
 (	
 	.clk				(clk),
 	.rst				(rst),
-	.led_if			(top_itf.led),
+	.led_if				(top_itf.led),
 	
 	.o_loadX			(o_loadX),
 	.o_loadY			(o_loadY),
 	.o_ready			(o_ready),
 	.o_error			(o_error)
+);
+
+TOP_bcd_7seg	TOP_BCD 
+(
+	.bcd_if				(top_itf.bcd),
+	
+	.out_displays_result		(o_displays_result),
+	//.out_displays_remainder	(o_displays_remainder),
+	.out_Signo			(o_sign)
 );
 
 endmodule
